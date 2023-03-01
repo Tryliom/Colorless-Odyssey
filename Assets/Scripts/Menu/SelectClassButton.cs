957405a1549701ece3c0cba82ef4f82a29a0e59e
Alexis Haldy <alexishaldy@gmail.com>
2023-03-01 16:42:26 +0100

    Added select class menu

---
+++ Assets/Scripts/Menu/SelectClassButton.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectClassButton : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _textButton;
+    [SerializeField] private RectTransform _rectTransform;
+    [SerializeField] private RunData _runData;
+    
+    [Header("To Set")]
+    [SerializeField] private GameObject _classTypePrefab;
+
+    private bool _hovered;
+    private GameObject _classTypeSpawned;
+    
+    private Animator _animator;
+    
+    private static readonly int Running = Animator.StringToHash("Running");
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Spawn the classtype prefab above the button
+        Vector2 positionToSpawn = Camera.main.ScreenToWorldPoint(_rectTransform.position);
+        _classTypeSpawned = Instantiate(_classTypePrefab, positionToSpawn, Quaternion.identity);
+        
+        // Set the name of the button to the name of the class
+        _textButton.text = _classTypePrefab.GetComponent<ClassType>().Name;
+        
+        _animator = _classTypeSpawned.GetComponent<Animator>();
+        _classTypeSpawned.GetComponent<MovementController>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_runData.classType != _classTypePrefab)
+        {
+            _animator.SetBool(Running, false);
+        }
+    }
+    
+    public void OnClick()
+    {
+        _runData.classType = _classTypePrefab;
+        
+        _animator.SetBool(Running, true);
+    }
+}
--- Assets/Scripts/Menu/SelectClassMenu.cs
+++ Assets/Scripts/Menu/SelectClassMenu.cs
@@ -14,22 +14,14 @@ public class SelectClassMenu : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _descriptionText;
     
     [Header("Buttons")]
-    [SerializeField] private GameObject _mageButton;
-    [SerializeField] private GameObject _slimeButton;
     [SerializeField] private GameObject _startButton;
     
-    [Header("ClassTypes")]
-    [SerializeField] private GameObject _mage;
-    [SerializeField] private GameObject _slime;
-    
-    [Header("PlayerPosition")]
-    [SerializeField] private Transform _playerSpawnPoint;
-    
-    private GameObject _currentPlayer;
-    
+    private GameObject _lastClassType = null;
+
     // Start is called before the first frame update
     void Start()
     {
+        _runData.classType = null;
         _startButton.SetActive(false);
         _informationPanel.SetActive(false);
     }
@@ -37,40 +29,18 @@ public class SelectClassMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (_runData.classType != null)
+        if (_runData.classType != null && (_lastClassType == null || _runData.classType != _lastClassType.gameObject))
         {
+            _lastClassType = _runData.classType;
+            
             _startButton.SetActive(true);
             _informationPanel.SetActive(true);
             
-            if (_runData.classType == _mage)
-            {
-                _slimeButton.SetActive(false);
-            }
-            else
-            {
-                _mageButton.SetActive(false);
-            }
+            _nameText.text = _lastClassType.GetComponent<ClassType>().Name;
+            _descriptionText.text = _lastClassType.GetComponent<ClassType>().Description;
         }
     }
-    
-    public void SelectMage()
-    {
-        _runData.classType = _mage;
-        
-        _nameText.text = _mage.GetComponent<ClassType>().Name;
-        _descriptionText.text = _mage.GetComponent<ClassType>().Description;
-        
-        // Remove old player if it exists and create new one with new class
-    }
-    
-    public void SelectSlime()
-    {
-        _runData.classType = _slime;
-        
-        _nameText.text = _slime.GetComponent<ClassType>().Name;
-        _descriptionText.text = _slime.GetComponent<ClassType>().Description;
-    }
-    
+
     public void StartGame()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("Playtest");
